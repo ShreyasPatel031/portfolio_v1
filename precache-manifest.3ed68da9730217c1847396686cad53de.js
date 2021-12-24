1e91533c4d6ca59197027b6232a327ad256a84b9
ShreyasPatel031 <shreyashfs@gmail.com>
2021-12-24 23:27:58 +0530

    Updates

---
+++ precache-manifest.3ed68da9730217c1847396686cad53de.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bb4730691f39cf357c82159b16cc24",
+    "url": "/portfolio_v1/index.html"
+  },
+  {
+    "revision": "adecaecc5fe8898720e2",
+    "url": "/portfolio_v1/static/css/main.ede5d7d6.chunk.css"
+  },
+  {
+    "revision": "40ddf591fb0f82f48547",
+    "url": "/portfolio_v1/static/js/2.4ff1524d.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/portfolio_v1/static/js/2.4ff1524d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adecaecc5fe8898720e2",
+    "url": "/portfolio_v1/static/js/main.a7c57cd9.chunk.js"
+  },
+  {
+    "revision": "dbdf828727a911cd16a0",
+    "url": "/portfolio_v1/static/js/runtime-main.faa108cd.js"
+  },
+  {
+    "revision": "f552f53baadac68a8dadbd05b36fe779",
+    "url": "/portfolio_v1/static/media/Data processing-pana.f552f53b.svg"
+  },
+  {
+    "revision": "3e14c45697d44932ffc336ffd5fefe21",
+    "url": "/portfolio_v1/static/media/Financial data-amico.3e14c456.svg"
+  },
+  {
+    "revision": "fe181accd67fc3af8dc7cfd759631764",
+    "url": "/portfolio_v1/static/media/Grades.fe181acc.svg"
+  },
+  {
+    "revision": "09f8e817e1fdbc8c103c79c84a7ffc0b",
+    "url": "/portfolio_v1/static/media/logo-higlighted.09f8e817.png"
+  },
+  {
+    "revision": "552037cd55d4d0d228c1c8d06b6140b6",
+    "url": "/portfolio_v1/static/media/logo-original.552037cd.png"
+  }
+]);
